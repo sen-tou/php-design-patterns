9bf7c5c20a5c15567c58fb21604c8c3eedeaef50
Steve <beyer_steve@outlook.de>
2022-02-12 16:29:54 +0100

    remove all abstract factory files

--- src/AbstractFactory/FileExporterFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Stvbyr\PhpDesignPatterns\AbstractFactory;
-
-class FileExporterFactory
-{
-}
--- src/AbstractFactory/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Stvbyr\PhpDesignPatterns\AbstractFactory\Order;
-
-require_once __DIR__.'/../../vendor/autoload.php';
-
-$order = new Order('ABC123345', 'James Holden', 9900);
